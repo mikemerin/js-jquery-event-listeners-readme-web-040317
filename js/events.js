e3c055a34a36b68a27f025498b2072d910cba2af
Mike Merin <mikemerinweather@gmail.com>
2017-05-17 13:29:46 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,23 @@
-//define functions here
+function getIt() { $('p').on("click", () => alert("hey!") ) }
+
+function frameIt() { $('img').on("load", () => $('img').addClass('tasty') ) }
+
+function pressIt() { $('#typing').on("keydown", (e) => {
+    if (e.which === 71 ) { alert("you pressed G") }
+    }
+  )
+}
+
+function submitIt() {
+  $("form").on("submit", () => alert("Your form is going to be submitted now.")
+) }
+
 
 $(document).ready(function(){
 
-// call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
